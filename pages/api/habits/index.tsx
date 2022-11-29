622d7aa7afff2e675d1a909ff6eb2d34edc0236c
Linus BALLS <linus.bolls@code.berlin>
2022-11-29 21:40:58 +0100

    fix: create habits.json if it does not exist

--- pages/api/habits/index.tsx
+++ pages/api/habits/index.tsx
@@ -1,15 +1,27 @@
-import { promises } from "fs"
+import { tmpdir } from "os"
+
+import { promises, existsSync } from "fs"
 import { join } from "path"
 
 import type { NextApiRequest, NextApiResponse } from 'next'
 import nc from 'next-connect';
 
-const HABITS_PATH = join(__dirname, "../../../../public/habits.json")
+const HABITS_PATH = join(tmpdir(), "habits.json")
 
 const handler = nc()
 
 handler.get(async (req: NextApiRequest, res: NextApiResponse<Record<string, string[]>>) => {
 
+    if (!existsSync(HABITS_PATH)) {
+
+        const exists = existsSync("/var/task/public/")
+
+        res.json({ path: [exists ? "exists" : "does not"] })
+
+        return
+
+        await promises.writeFile(HABITS_PATH, `{ "morning": [ "mir dick einen runterholen" ] }`, { flag: "wx" },)
+    }
     const data = (await promises.readFile(HABITS_PATH)).toString()
 
     const habits = JSON.parse(data)
@@ -19,8 +31,6 @@ handler.get(async (req: NextApiRequest, res: NextApiResponse<Record<string, stri
 handler.put(async (req: NextApiRequest, res: NextApiResponse) => {
 
     const markdownHabits: Record<string, string> = JSON.parse(req.body)
-
-    console.log("markdownHabits:", markdownHabits)
 
     const jsonHabits = Object.fromEntries(Object.entries(markdownHabits).map(([habitCategory, habitStr]) => ([habitCategory, habitStr.slice(2).split("\n- ")])))
 
