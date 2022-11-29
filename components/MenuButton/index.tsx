ef0b4d16198fdafc9b20a26ffa04a2f913d33836
raphaelsmosaic <98910367+raphaelsmosaic@users.noreply.github.com>
2022-11-29 23:12:27 +0100

    removed menu button

--- components/MenuButton/index.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link"
-import style from "./menuButton.module.css"
-
-export default function MenuButton() {
-
-    return <Link href="/" className={style.menuButton}>
-        Back
-    </Link>
-}
